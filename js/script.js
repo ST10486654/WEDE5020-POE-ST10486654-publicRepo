d74508b48ae172e13157893d9382f4325a49fd72
ST10486654 <ST10486654@VCCONNECT.EDU.ZA>
2025-11-04 21:40:08 +0200

    details of js file
    Describing what kind of code I will write on the script.js file.

---
+++ js/script.js
@@ -0,0 +1,14 @@
+/* ============================================================
+   CoffeeBox Website (Part 3: JavaScript  & SEO Enhancements)
+   Author: Dumolwenkosi MGwetu | Date: 04 Nov 2025
+   ============================================================
+   PURPOSE:
+   This script adds all JavaScript-based interactivity,
+   validation, and dynamic behavior for the CoffeeBox website.
+
+    UX & UI Principles Applied:
+   - Visibility of system status (feedback on actions)
+   - Consistency & standards (same interaction patterns)
+   - Error prevention & clear recovery
+   - Aesthetic minimalism with motion for delight
+   ============================================================ */
